d88a9d3a6cf0ea1a8b622143a6deb09e24d63f06
Your New Name <e.m.i.com>
2023-11-22 17:37:37 +0530

    'commit'

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Post;
+use http\Env\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'=>$this->id,
+            'title'=>$this->title,
+            'body'=>$this->body,
+        ];
+    }
+
+}
